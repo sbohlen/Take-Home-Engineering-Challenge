6981a55bbfa7342b4ead4a987ae179a93e49f8bc
sbohlen <sbohlen@gmail.com>
2021-12-03 15:22:54 -0500

    initial data ingestion post-processing T-SQL

---
+++ sql/populate_borough_lookup_table.sql
@@ -0,0 +1,1 @@
+insert into dbo.borough_lookup (borough) select distinct Borough from dbo.taxi_zone_lookup
---
+++ sql/update_tripdata_tables_with_borough_ids.sql
@@ -0,0 +1,19 @@
+update green_tripdata 
+set
+	PUBoroughID = (select blu.id from borough_lookup blu inner join taxi_zone_lookup tlz on blu.borough = tlz.Borough where tlz.LocationID = PULocationID),
+	DOBoroughID = (select blu.id from borough_lookup blu inner join taxi_zone_lookup tlz on blu.borough = tlz.Borough where tlz.LocationID = DOLocationID)
+
+update yellow_tripdata 
+set
+	PUBoroughID = (select blu.id from borough_lookup blu inner join taxi_zone_lookup tlz on blu.borough = tlz.Borough where tlz.LocationID = PULocationID),
+	DOBoroughID = (select blu.id from borough_lookup blu inner join taxi_zone_lookup tlz on blu.borough = tlz.Borough where tlz.LocationID = DOLocationID)
+
+update fhv_tripdata 
+set
+	PUBoroughID = (select blu.id from borough_lookup blu inner join taxi_zone_lookup tlz on blu.borough = tlz.Borough where tlz.LocationID = PULocationID),
+	DOBoroughID = (select blu.id from borough_lookup blu inner join taxi_zone_lookup tlz on blu.borough = tlz.Borough where tlz.LocationID = DOLocationID)
+
+update fhvhv_tripdata 
+set
+	PUBoroughID = (select blu.id from borough_lookup blu inner join taxi_zone_lookup tlz on blu.borough = tlz.Borough where tlz.LocationID = PULocationID),
+	DOBoroughID = (select blu.id from borough_lookup blu inner join taxi_zone_lookup tlz on blu.borough = tlz.Borough where tlz.LocationID = DOLocationID)
