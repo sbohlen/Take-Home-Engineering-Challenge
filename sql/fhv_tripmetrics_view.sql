4a5ae958cbbc8957f7ee8405e8c64e14b8cbc389
sbohlen <sbohlen@gmail.com>
2021-12-03 16:55:30 -0500

    queries for views capturing green cab and fhv trips

---
+++ sql/fhv_tripmetrics_view.sql
@@ -0,0 +1,47 @@
+select td.PUBoroughID, td.DOBoroughID,
+
+--subselect to resolve the origin borough based on id of pick-up zone
+(select blu.Borough from borough_lookup blu where blu.id = td.PUBoroughID) as origin,
+
+--subselect to resolve the destintation borough based on id of drop-off zone
+(select blu.Borough from borough_lookup blu where blu.id = td.DOBoroughID) as destination,
+
+--extract the 0-23 hour value from the pick-up date/time
+DATEPART(HOUR, td.pickup_datetime) as tripHour,
+
+--calculate the AVG for all trips having the same hour
+(select top 1 AVG(DATEDIFF(minute, td.pickup_datetime, td.dropoff_datetime)) as answer
+   	from fhv_tripdata tdtimes
+	where
+	DATEPART(HOUR, tdtimes.pickup_datetime) = DATEPART(HOUR, td.pickup_datetime)
+	and tdtimes.PUBoroughID = td.PUBoroughID
+	and tdtimes.DOBoroughID = td.DOBoroughID
+	order by answer desc
+    ) as averageDuration,
+
+--calculate the MIN for all trips having the same hour
+(select top 1 MIN(DATEDIFF(minute, td.pickup_datetime, td.dropoff_datetime)) as answer
+   	from fhv_tripdata tdtimes
+	where
+	DATEPART(HOUR, tdtimes.pickup_datetime) = DATEPART(HOUR, td.pickup_datetime)
+	and tdtimes.PUBoroughID = td.PUBoroughID
+	and tdtimes.DOBoroughID = td.DOBoroughID
+	order by answer desc
+    ) as minimumDuration,
+
+--calculate the MAX for all trips having the same hour
+(select top 1 MAX(DATEDIFF(minute, td.pickup_datetime, td.dropoff_datetime)) as answer
+   	from fhv_tripdata tdtimes
+	where
+	DATEPART(HOUR, tdtimes.pickup_datetime) = DATEPART(HOUR, td.pickup_datetime)
+	and tdtimes.PUBoroughID = td.PUBoroughID
+	and tdtimes.DOBoroughID = td.DOBoroughID
+	order by answer desc
+    ) as maximumDuration
+
+from fhv_tripdata td
+
+--exclude the unresolvable "zone id zero" entries (dirty data!)
+where td.PULocationID != 0 and td.DOLocationID !=0
+
+group by DATEPART(HOUR, td.pickup_datetime), td.PUBoroughID, td.DOBoroughID
---
+++ sql/green_tripmetrics_view.sql
@@ -0,0 +1,74 @@
+select td.PUBoroughID, td.DOBoroughID,
+
+--subselect to resolve the origin borough based on id of pick-up zone
+(select blu.Borough from borough_lookup blu where blu.id = td.PUBoroughID) as origin,
+
+--subselect to resolve the destintation borough based on id of drop-off zone
+(select blu.Borough from borough_lookup blu where blu.id = td.DOBoroughID) as destination,
+
+--extract the 0-23 hour value from the pick-up date/time
+DATEPART(HOUR, td.lpep_pickup_datetime) as tripHour,
+
+--calculate the AVG for all trips having the same hour
+(select AVG(tdcosts.total_amount)
+   	from green_tripdata tdcosts
+	where tdcosts.total_amount > 0
+    and DATEPART(HOUR, tdcosts.lpep_pickup_datetime) = DATEPART(HOUR, td.lpep_pickup_datetime)
+	and tdcosts.PUBoroughID = td.PUBoroughID
+	and tdcosts.DOBoroughID = td.DOBoroughID
+    ) as averageCost,
+
+--calculate the MIN for all trips having the same hour
+(select MIN(tdcosts.total_amount)
+   	from green_tripdata tdcosts
+	where tdcosts.total_amount > 0
+    and DATEPART(HOUR, tdcosts.lpep_pickup_datetime) = DATEPART(HOUR, td.lpep_pickup_datetime)
+	and tdcosts.PUBoroughID = td.PUBoroughID
+	and tdcosts.DOBoroughID = td.DOBoroughID
+    ) as minimumCost,
+
+--calculate the MAX for all trips having the same hour
+(select MAX(tdcosts.total_amount)
+   	from green_tripdata tdcosts
+	where tdcosts.total_amount > 0
+    and DATEPART(HOUR, tdcosts.lpep_pickup_datetime) = DATEPART(HOUR, td.lpep_pickup_datetime)
+	and tdcosts.PUBoroughID = td.PUBoroughID
+	and tdcosts.DOBoroughID = td.DOBoroughID
+    ) as maximumCost,
+
+--calculate the AVG for all trips having the same hour
+(select top 1 AVG(DATEDIFF(minute, td.lpep_pickup_datetime, td.lpep_dropoff_datetime)) as answer
+   	from green_tripdata tdtimes
+	where
+	DATEPART(HOUR, tdtimes.lpep_pickup_datetime) = DATEPART(HOUR, td.lpep_pickup_datetime)
+	and tdtimes.PUBoroughID = td.PUBoroughID
+	and tdtimes.DOBoroughID = td.DOBoroughID
+	order by answer desc
+    ) as averageDuration,
+
+--calculate the MIN for all trips having the same hour
+(select top 1 MIN(DATEDIFF(minute, td.lpep_pickup_datetime, td.lpep_dropoff_datetime)) as answer
+   	from green_tripdata tdtimes
+	where
+	DATEPART(HOUR, tdtimes.lpep_pickup_datetime) = DATEPART(HOUR, td.lpep_pickup_datetime)
+	and tdtimes.PUBoroughID = td.PUBoroughID
+	and tdtimes.DOBoroughID = td.DOBoroughID
+	order by answer desc
+    ) as minimumDuration,
+
+--calculate the MAX for all trips having the same hour
+(select top 1 MAX(DATEDIFF(minute, td.lpep_pickup_datetime, td.lpep_dropoff_datetime)) as answer
+   	from green_tripdata tdtimes
+	where
+	DATEPART(HOUR, tdtimes.lpep_pickup_datetime) = DATEPART(HOUR, td.lpep_pickup_datetime)
+	and tdtimes.PUBoroughID = td.PUBoroughID
+	and tdtimes.DOBoroughID = td.DOBoroughID
+	order by answer desc
+    ) as maximumDuration
+
+from green_tripdata td
+
+--exclude the unresolvable "zone id zero" entries (dirty data!)
+where td.PULocationID != 0 and td.DOLocationID !=0
+
+group by DATEPART(HOUR, td.lpep_pickup_datetime), td.PUBoroughID, td.DOBoroughID
